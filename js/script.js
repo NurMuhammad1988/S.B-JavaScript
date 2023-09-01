1a32a5d1a0e21f1f0a8369d6459ab79c4389c10b
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-09-01 21:21:05 +0500

    hello github i miss you

---
+++ js/script.js
@@ -0,0 +1,107 @@
+"use strict"; //qattiy rejim eski kodlarni ishlatmeydi
+
+// number = 10
+// console.log(number);
+
+// console.log(numberr);
+// let numberr = 10;
+// console.log(numberr);
+// let number = 10;
+// console.log(number);
+
+// const a = 5;
+// console.log(a);
+
+// number = 20;
+
+// console.log(number);
+// //////////////////////////////////////////////////////
+// let number = 10;
+// const borderColor = "red";
+
+// const person = {
+//     name: "Nur",
+// };
+
+// person.name = "Yorov";
+// console.log(person);
+// ////////////////////////////////////////////////////////
+
+// var name = "Nur";
+// name = "Far";
+// console.log(name);
+
+// console.log(clientName);
+// var clientName ="Nur"
+// console.log(clientName);
+
+/////////////////////////////////////////////////////////
+
+// let number = 10;
+// const borderColor = 'red';
+// console.log(number);
+// number = 20;
+// console.log(number);
+/////////////////////////////////////////////////////////////
+// console.log(number );// hato
+// let number = 10;
+// console.log(number);
+
+// const borderColor = "red";
+// console.log(borderColor);
+
+// const person ={
+//     name:"Nur",
+// }
+
+// person.name = "Doston"
+// console.log(person);
+
+// var name = "Nur";// old version always work dont use
+// console.log(name);
+
+// console.log(clientName);
+// var clientName = "Nur"
+// console.log(clientName);
+
+//////////////// MALUMOT TURLARI ///////////////////////////////////
+
+//Number malumot turlari
+// let number = 5.6; //data tipe number
+// console.log(-8 / 0); // infinitiy
+// console.log("Nur" * 8); // nan
+// console.log(number);
+
+// String malumot turlari
+// const clientNamename = "Nur111"; //stringda qo'shtirnoqlarni deyarli farqi yoq
+// console.log(clientNamename);
+
+//BOOLING malumot turi mantiqiy
+// const isMrried1 = true;// ha to'g'ri
+// const isMrried2  = false;//yo'q noto'g'ri
+
+//NAN malumot turi yani  null yo'q o'zgaruvchini chaqirganda null yani nan chiqadi
+// console.log(clientAge);
+
+//undefined o'zgaruvchiga qiymat berilmasdan chaqirilganda undifined chiqadi
+// let und;
+// console.log(und);
+
+//object malumot turi
+// const thief = {
+    //age key yani kalit 30 qiymat yani value
+    // age: 30, //number malumot turi
+    // jacket: "black", //string malumot turi
+    // islong: false, //boolean malumot turi mantiqiy
+    // color: "white", //string malumot turi
+    // shoes: "military boots", //string malumot turi
+// };
+// console.log(thief.age); //object ichida gi malumotlarni alohida alohidaham olish mumkun
+// console.log(thief); //bu birinchi yo'li
+// console.log(thief["islong"]); //bu ikkinchi yo'li
+
+//Array malumot turi
+//arrayda jsni ozi key berib chiqadi va bu doim noldan boshlanadi
+//                0       1        2     3   4  5  6   7
+// const colors = ["red", "black", "green",10,[11,12,23],{}];
+// console.log(colors[0]);
