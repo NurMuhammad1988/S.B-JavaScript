924e51ce0f47c54f0256fef662ed506208ef5990
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-09-30 05:28:28 +0500

    3-modul loyiha classes

--- 3-modul loyiha/js/script.js
+++ 3-modul loyiha/js/script.js
@@ -138,9 +138,79 @@ window.addEventListener("DOMContentLoaded", () => {
             document.documentElement.scrollHeight
         ) {
             openModal();
-            window.removeEventListener('scroll' , showModalByScroll)
+            window.removeEventListener("scroll", showModalByScroll);
         }
-    } 
+    }
 
-    window.addEventListener('scroll', showModalByScroll)
+    window.addEventListener("scroll", showModalByScroll);
+
+    //class
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 11000;
+            this.changeToUZS();
+        }
+
+        changeToUZS() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement("div");
+            element.innerHTML = `
+            
+            <div class="menu__item">
+            <img src=${this.src} alt=${this.alt} />
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+              <div class="menu__item-cost">Price:</div>
+              <div class="menu__item-total"><span>${this.price}</span> uzs/month</div>
+            </div>
+          </div>
+
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/1.png",
+        "usual",
+        "Plan 'Usual'",
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem praesentium ab cupiditate beatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis harum voluptatum in.",
+        10,
+        ".menu .container"
+    ).render();
+
+
+    new MenuCard(
+        "img/tabs/2.jpg",
+        "plan",
+        "Plan 'Premium'",
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem praesentium ab cupiditate beatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis harum voluptatum in.",
+        20,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/3.jpg",
+        "vip",
+        "Plan 'VIP'",
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem praesentium ab cupiditate beatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis harum voluptatum in.",
+        30,
+        ".menu .container"
+    ).render();
+
+
+    
+
+
 });
